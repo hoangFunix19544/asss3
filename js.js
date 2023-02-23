5b55629a44f44202797bda37ddf7615c7f236e12
hoangFunix19544 <118522044+hoangFunix19544@users.noreply.github.com>
2023-02-23 10:38:38 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,61 @@
+
+// div3
+function hiendiv3(){ if(document.getElementById("div3").style.display=="none"){
+    document.getElementById("div3").style.display="block";
+    document.getElementById("viewmore1").innerText="View less";}
+    else if(document.getElementById("div3").style.display==""){
+        document.getElementById("div3").style.display="block";
+        document.getElementById("viewmore1").innerText="View less";
+    }else
+    {  document.getElementById("div3").style.display="none";
+    document.getElementById("viewmore1").innerText="View more";}}
+// div4
+function hiendiv4(){ if(document.getElementById("div4").style.display=="none"){
+    document.getElementById("div4").style.display="block";
+    document.getElementById("viewmore2").innerText="View less";}
+    else if(document.getElementById("div4").style.display==""){
+        document.getElementById("div4").style.display="block";
+        document.getElementById("viewmore2").innerText="View less";
+    }else
+    {  document.getElementById("div4").style.display="none";
+    document.getElementById("viewmore2").innerText="View more";}}
+    // div5
+    function hiendiv5(){ if(document.getElementById("div5").style.display=="none"){
+        document.getElementById("div5").style.display="block";
+        document.getElementById("viewmore3").innerText="View less";}
+        else if(document.getElementById("div5").style.display==""){
+            document.getElementById("div5").style.display="block";
+            document.getElementById("viewmore3").innerText="View less";
+        }else
+        {  document.getElementById("div5").style.display="none";
+        document.getElementById("viewmore3").innerText="View more";}}
+// div6
+function hiendiv6(){ if(document.getElementById("div6").style.display=="none"){
+    document.getElementById("div6").style.display="block";
+    document.getElementById("viewmore4").innerText="View less";}
+    else if(document.getElementById("div6").style.display==""){
+        document.getElementById("div6").style.display="block";
+        document.getElementById("viewmore4").innerText="View less";
+    }else
+    {  document.getElementById("div6").style.display="none";
+    document.getElementById("viewmore4").innerText="View more";}}
+    // div7
+    function hiendiv7(){ if(document.getElementById("div7").style.display=="none"){
+        document.getElementById("div7").style.display="block";
+        document.getElementById("viewmore5").innerText="View less";}
+        else if(document.getElementById("div7").style.display==""){
+            document.getElementById("div7").style.display="block";
+            document.getElementById("viewmore5").innerText="View less";
+        }else
+        {  document.getElementById("div7").style.display="none";
+        document.getElementById("viewmore5").innerText="View more";}}
+        // div8
+        function hiendiv8(){ if(document.getElementById("div8").style.display=="none"){
+            document.getElementById("div8").style.display="block";
+            document.getElementById("viewmore6").innerText="View less";}
+            else if(document.getElementById("div8").style.display==""){
+                document.getElementById("div8").style.display="block";
+                document.getElementById("viewmore6").innerText="View less";
+            }else
+            {  document.getElementById("div8").style.display="none";
+            document.getElementById("viewmore6").innerText="View more";}}
